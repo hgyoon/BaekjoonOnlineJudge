179a20d11452ce21f1bffbcff94db6639ae12aa9
Hyungu Yoon <hgyoon@umich.edu>
2023-04-02 10:43:48 +0900

    solved

---
+++ 1181.py
@@ -0,0 +1,17 @@
+'''
+strip() was used to get rid of "\n"s at the end of each lines
+implementation without using set() and sort(key = len)
+'''
+import sys
+N = int(sys.stdin.readline())
+words = [[] for _ in range(0,51)]
+for _ in range(N):
+	word = sys.stdin.readline().strip()
+	if word not in words[len(word)-1]:
+	    words[len(word)-1].append(word)
+for i in words:
+    i.sort()
+for j in words:
+	if len(j) != 0:
+	    for k in j:
+	        print(k)
